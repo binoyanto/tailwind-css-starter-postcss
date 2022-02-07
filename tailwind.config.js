b82409bea4c658d85aa353c0a1e8949e9bd1d0fe
Binoy Anto <binoyanto@gmail.com>
2022-02-07 11:32:04 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,33 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: {
     content: ['./public/**/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'cyan': colors.cyan,
+        'teal': colors.teal
+      }
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+
+  plugins: [
+    plugin(function ({ addBase }) {
+      addBase({
+        '@font-face': {
+            fontFamily: 'Inter-roman.var',
+            src: 'url(https://cdnjs.cloudflare.com/ajax/libs/inter-ui/3.13.1/Inter%20(web)/Inter-roman.var.woff2) format(\'woff2\')'
+        }
+      })
+    }),
+  ],
 }
